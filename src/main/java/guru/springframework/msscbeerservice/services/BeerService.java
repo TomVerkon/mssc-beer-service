1884230e56dc6a90dfe2b238b70215efcbe05ee7
Tom Verkon <tverkon@usa.net>
2019-09-08 19:38:37 -0400

    Added List of Beers, enhance with inventory information closes #1

--- src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
+++ src/main/java/guru/springframework/msscbeerservice/services/BeerService.java
@@ -1,8 +1,12 @@
 package guru.springframework.msscbeerservice.services;
 
 import guru.springframework.msscbeerservice.web.model.BeerDto;
+import guru.springframework.msscbeerservice.web.model.BeerPagedList;
+import guru.springframework.msscbeerservice.web.model.BeerStyleEnum;
 
 import java.util.UUID;
+
+import org.springframework.data.domain.PageRequest;
 
 /**
  * Created by jt on 2019-06-06.
@@ -13,4 +17,6 @@ public interface BeerService {
     BeerDto saveNewBeer(BeerDto beerDto);
 
     BeerDto updateBeer(UUID beerId, BeerDto beerDto);
+
+    BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, PageRequest pageRequest);
 }
